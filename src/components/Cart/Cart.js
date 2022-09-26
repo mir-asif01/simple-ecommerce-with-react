70acceb6f5eeaf94a3384461682aac7f3844fbc2
asif3062 <asifbsb2002@gmail.com>
2022-09-26 11:48:07 +0600

    cart calculation added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    let tax = Number(((10/100)*total).toFixed(2));
+    let grandTotal = (total+shipping+tax)
     return (
         <div>
             <h3>Order Summery</h3>
             <h4>Item added : {cart.length}</h4>
-            <h4>Total Price : 0</h4>
+            <h4>Total Price : ${total}</h4>
+            <h4>Total Shipping Price : ${shipping}</h4>
+            <h4>Tax : ${tax}</h4>
+            <h4>Grand Total : ${grandTotal}</h4>
         </div>
     );
 };
